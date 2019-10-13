54c49da16846c2c088d3237c84119df081c28428
kulas115 <kulas115@gmail.com>
2019-10-13 22:12:51 +0200

    Add simple vue quiz app

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron>
+      <template v-slot:lead>
+        {{ currentQuestion.question | decoder }}
+      </template>
+
+      <hr class="my-4">
+
+      <b-list-group>
+        <b-list-group-item
+          v-for="(answer, index) in answers"
+          :key="index"
+          @click="selectAnswer(index)"
+          :class="answerClass(index)"
+        >
+          {{ answer | decoder }}
+        </b-list-group-item>
+      </b-list-group>
+
+      <b-button
+        variant="primary"
+        @click="submitAnswer"
+        :disabled="selectedIndex === null || answered"
+      >
+        Submit
+      </b-button>
+
+      <b-button @click="next" variant="success" href="#">
+        Next
+      </b-button>
+
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+  import _ from 'lodash'
+
+  export default {
+    props: {
+      currentQuestion: Object,
+      next: Function,
+      increment: Function
+    },
+    data() {
+      return {
+        selectedIndex: null,
+        shuffledAnswers: [],
+        answered: false,
+        correctIndex: null
+      }
+    },
+    computed: {
+      answers() {
+        let answers = [...this.currentQuestion.incorrect_answers]
+        answers.push(this.currentQuestion.correct_answer)
+        return answers
+      }
+    },
+    watch: {
+      currentQuestion: {
+        immediate: true,
+        handler() {
+          this.selectedIndex = null
+          this.answered = false
+          this.shuffleAnswers()
+        }
+      }
+    },
+    filters: {
+      decoder(string) {
+        const textArea = document.createElement('textarea');
+        textArea.innerHTML = string;
+        return textArea.value;
+      }
+    },
+    methods: {
+      selectAnswer(index) {
+        this.selectedIndex = index
+        console.log(index)
+      },
+      submitAnswer() {
+        let isCorrect = false
+
+        if (this.selectedIndex === this.correctIndex) {
+          isCorrect = true
+        }
+
+        this.answered = true
+        this.increment(isCorrect)
+      },
+      shuffleAnswers() {
+        let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+        this.shuffledAnswers = _.shuffle(answers)
+        this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer)
+      },
+      answerClass(index) {
+        let answerClass = ''
+
+        if (!this.answered && this.selectedIndex === index) {
+          answerClass = 'selected'
+        } else if (this.answered && this.correctIndex === index) {
+          answerClass = 'correct'
+        } else if (this.answered &&
+          this.selectedIndex === index &&
+          this.correctIndex !== index) {
+          answerClass = 'incorrect'
+        }
+
+        return answerClass
+      }
+    },
+    mounted() {
+      console.log(this.question)
+    }
+  }
+</script>
+
+<style scoped>
+  .list-group {
+    margin-bottom: 15px;
+  }
+
+  .list-group-item:hover {
+    background-color: #eee;
+    cursor: pointer;
+  }
+
+  .btn {
+    margin: 0 5px;
+  }
+
+  .selected {
+    background-color: lightblue;
+  }
+
+  .correct {
+    background-color: lightgreen;
+  }
+
+  .incorrect {
+    background-color: red;
+  }
+</style>
